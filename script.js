03d823c75a63c4952d788745c5133458e0c10b86
Avery Dyer <avedyer@gmail.com>
2021-10-12 11:58:19 -0400

    changed library display to create div for each book

--- script.js
+++ script.js
@@ -1,36 +1,56 @@
 let myLibrary = [];
 
-function Book (title, author, pages, read) {
+function Book (title, author, pages, read, id) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
+    this.id = id
+}
+
+let writeID = function(){
+    let id = Math.floor(Math.random(10**16))
+    for (let book in myLibrary) {
+        if (book.id === id)
+        writeID();
+    }
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read);
+    const newBook = new Book(title, author, pages, read, writeID());
     myLibrary.push(newBook);
 }
 
-let displayLibrary = () => {console.table(myLibrary)}
-
-const book1 = new Book("Goldfinch", "Mary Murray", 586, false);
-const book2 = new Book("After Dawn", "John Maxcock", 194, true);
-
-myLibrary.push(book1, book2);
-
-let tbody = document.querySelector("tbody")
+let bookshelf = document.getElementById('bookshelf')
 
 let displayLibrary = () => {    
     myLibrary.forEach(function (book) {
 
-        let newRow = tbody.insertRow();
+        let newTitleElement = document.createElement('h2');
+        newTitleElement.classList.add('title');
+        newTitleElement.innerHTML = book.title;
 
-        let i = 0;
-        for(const key in book ) {
-            let newCell = newRow.insertCell(i);
-            newCell.innerHTML = book[key];
-            ++i;
-        }
+        let newAuthorElement = document.createElement('h3');
+        newAuthorElement.classList.add('author');
+        newAuthorElement.innerHTML = book.author;
+
+        let newPagesElement = document.createElement('h3');
+        newPagesElement.classList.add('pages');
+        newPagesElement.innerHTML = book.pages;
+
+        let newReadElement = document.createElement('h3');
+        newReadElement.classList.add('read');
+        newReadElement.innerHTML = book.read ? "read" : "unread";
+
+        let newBookElement = document.createElement('div')
+        newBookElement.classList.add('book')
+        newBookElement.append(newTitleElement, newAuthorElement, newPagesElement, newReadElement);
+
+        bookshelf.appendChild(newBookElement);
     });
 }
+
+const book1 = new Book("Goldfinch", "Mary Murray", 586, false, writeID());
+const book2 = new Book("After Dawn", "John Maxcock", 194, true, writeID());
+
+myLibrary.push(book1, book2);
