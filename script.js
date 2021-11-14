04b5add7ec2840f75bd4c7440ccf2255ff3c4b38
avedyer <avedyer@gmail.com>
2021-11-14 17:12:24 -0500

    removed comment block

--- script.js
+++ script.js
@@ -54,16 +54,6 @@ newBookSubmit.onclick = () => {
      }
 }
 
-/*
-function Book (title, author, pages, read, id) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.id = id
-}
-*/
-
 class Book {
     constructor(title, author, pages, read, id) {
         this.title = title
